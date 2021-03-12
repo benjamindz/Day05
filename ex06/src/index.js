686f4263fe0f7107018740de1d90430292ac0895
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-12 20:34:01 +0100

    created a variable using two string and a variable

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    var myName = "Benjamin";
+    var myString = "My name is " + myName + " and I am well!"
+
+
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
